55186f8dc5288f2d34fea55264335dc4801362c7
smdtoy <2101395@s.asojuku.ac.jp>
2022-11-04 12:00:26 +0900

    ログイン画面css

---
+++ new_user/new_user.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head>
+<title>会員登録画面</title>
+</head>
+<body>
+性別
+<input type="radio" name="gender" value="male">男性
+<input type="radio" name="gender" value="female">女性
+<input type="radio" name="gender" value="sonota">その他<br>
+
+生年月日
+<input type="date" name="calendar" max="9999-12-31" id="birth">
+<form method="post" action="test.html"><br>
+
+氏名
+姓<input type="text" maxlength="100">
+名<input type="text" maxlength="100"><<br>
+
+メールアドレス
+<input type="text" maxlength="254"><br>
+
+パスワード
+<input type="password" maxlength="20"><br>
+
+住所
+<input type="text" maxlength="200"><br>
+
+電話番号
+<input type="text" maxlength="21"><br>
+
+</form>
+</body>
+</html>
