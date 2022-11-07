201bb42dbab8c069292570b46d83bcab3b8fa3a7
ItoRyusei <2101426@s.asojuku.ac.jp>
2022-11-07 10:32:58 +0900

    DBManager作成
    機能追加

--- DBManager.php
+++ DBManager.php
@@ -18,7 +18,7 @@ class DBManager {
         $ps -> execute();
     }
 
-    //商品名検索(名前)
+    //商品検索(名前)
     public function getShohinTblByName($getshohinName) {
         $pdo = $this -> dbConnect();
         $sql =  "SELECT * FROM shohin_tbl WHERE shohin_name LIKE ? ORDER BY shohin_id";
@@ -28,5 +28,44 @@ class DBManager {
         $searchArray = $ps -> fetchAll();
         return $searchArray;
     }
+
+    //商品検索(県別)
+    public function getShohinTblByKen($getkeNname) {
+        $pdo = $this -> dbConnect();
+        $sql =  "SELECT * FROM shohin_tbl WHERE kyushu = ? ORDER BY shohin_id";
+        $ps = $pdo -> prepare($sql);
+        $ps -> bindValue(1, $getkenName, PDO::PARAM_INT);
+        $ps -> execute();
+        $searchArray = $ps -> fetchAll();
+        return $searchArray;
+    }
+
+    //商品検索
+    public function getShohinTblByAttribute() {
+
+    }
+
+    //ユーザー登録内容表示
+    public function getUserTblByUid($getuid) {
+        $pdo = $this -> dbConnect();
+        $sql = "SELECT * FROM user_tbl WHERE user_id = ?";
+        $ps = $pdo -> prerare($sql);
+        $ps -> bindValue(1, $getuid, PDO::PARAM_STR);
+        $ps -> execute();
+        $searchArray =$ps -> fetchAll();
+        return $searchArray;
+    }
+
+    //商品カート表示
+    public function getCartTblByUid($getuid) {
+        $pdo = $this -> dbConnect();
+        $sql = "SELECT Stbl.shohin_name, Stbl.shohin_img, Stbl.price, SCtbl.total_price, SCtbl.item_kosu, Utbl.address FROM shohin_tbl AS Stbl 
+        INNER JOIN cart_tbl AS SCtbl ON Stbl.user_id = SCtbl.user_id INNER JOIN user_tbl AS Utbl ON SCtbl.user_id = Utbl.user_id";
+        
+    }
+
+    //商品カート登録
+
+    //商品カート削除
 }
 ?>
