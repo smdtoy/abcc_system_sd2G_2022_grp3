e411bdc85b699f31afc8f3b081213b5ee06b4128
smdtoy <2101395@s.asojuku.ac.jp>
2022-11-16 12:29:31 +0900

    トップ名変更

--- new_user/new_user.php
+++ new_user/new_user.php
@@ -104,7 +104,7 @@
             <h2 class="card-title">新規会員登録</h2>
         </div>
     </div>
-
+<from action="kakunin_user.php" method="post">
     <div class="card user-card">
         <div class="card-body">
             <ul class="list-group list-group-flush">
@@ -351,5 +351,6 @@
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</form>
 </body>
 </html>
--- top/Top_index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require 'Top.php';
-    $pdo = connect();
-    $st = $pdo->query("SELECT * FROM goods");
-    $goods = $st->fetchAll();
-    require 't_index.php';
-?>
