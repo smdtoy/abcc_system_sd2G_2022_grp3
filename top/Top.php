0ca947049b2e34b93765f4b33bb6333861f97e64
ItoRyusei <2101426@s.asojuku.ac.jp>
2022-11-16 12:42:31 +0900

    画面遷移

--- top/Top.php
+++ top/Top.php
@@ -23,7 +23,7 @@
                 <a class="navbar-brand" href="#" id="logo">SAKETOWN</a>
             </div>
             <div class="col-md-4 offset-md-2 col-sm-6">
-                <form class="d-flex align-items-center" action="#" method="post">
+                <form class="d-flex align-items-center" action="./shohin/shohin_word.php" method="post">
                     <div class="col-md-10">
                         <input class="form-control" type="text" placeholder="Search">
                     </div>
@@ -33,7 +33,7 @@
                 </form>
             </div>
             <div class="col-md-3 offset-md-1 col-sm-6">
-                <button type="button" class="btn btn-ligth"><i class="bi bi-cart2"></i></button>
+                <a class="btn btn-ligth" href="./shoppingcart/shopcart.php"><i class="bi bi-cart2"></i></a>
                 <button type="button" class="btn btn-ligth"><i class="bi bi-suit-heart"></i></button>
                 <button type="button" class="btn btn-ligth navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse" aria-controls="navbars1 navbars2 navbars3" aria-expanded="false"><i class="bi bi-list"></i></button>
             </div>
